b0e65bb608b778959418744adad1b24d5953db9e
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-03-13 22:25:41 +0100

    tryRunJavascriptFile and stuff It should execute it and stuff

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var vm = require("vm");
 var express = require("express");
 
 var basePath = process.argv[2];
@@ -22,15 +23,59 @@ app.get("/*", function(req, res) {
 
 function tryRunPage(page, req, res) {
   var filePath = basePath+"/pages"+page;
-  var javascriptFilePath = filePath+".js";
-  var htmlFilePath = filePath+".htm";
-  fs.readFile(htmlFilePath, function(err, data) {
+  tryRunJavascriptPage(filePath, req, res, function(error1) {
+    if(error1) {
+      tryRunHtmlPage(filePath, req, res, function(error2) {
+        if(error2) {
+          runErrorPage(error1, error2, req, res);
+	}
+      });
+    }
+  });
+}
+
+function tryRunJavascriptPage(page, req, res, callback) {
+  fs.readFile(page+".js", function(err, data) {
     if(err) {
-      res.end("Failed to read the file "+htmlFilePath+": "+JSON.stringify(err));
-      return;
+      return callback(err);
+    }
+    var module = {};
+    var context = vm.createContext({
+      module: {}
+    });
+    try {
+      vm.runInContext(data, context);
+      context.module.exports.call({
+        query: req.query
+      }, function(viewToRender, variables) {
+        fs.readFile(page+"/../../views/"+viewToRender, function(err, data) {
+          if(err) {
+            return callback(err);
+	  }
+	  res.end(data);
+	  callback();
+	});
+      });
+    }
+    catch(e) {
+      console.log("error", e);
+      return callback(e);
+    }
+  });
+}
+
+function tryRunHtmlPage(page, req, res, callback) {
+ fs.readFile(page+".htm", function(err, data) {
+    if(err) {
+      return callback(err);
     }
     res.end(data);
+    callback();
   });
+}
+
+function runErrorPage(error1, error2, req, res) {
+  res.end("Failed to read the file<br>"+JSON.stringify(error1)+"<br>"+JSON.stringify(error2));
 }
 
 app.listen(80);
