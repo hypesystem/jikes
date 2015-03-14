8d9a49bff15603a569a08701b67f2210e4d4fe7c
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-03-14 10:55:37 +0100

    Extract Jikes class

--- app.js
+++ app.js
@@ -1,96 +1,9 @@
 process.title = "jikes!";
 var basePath = process.argv[2] || ".";
 
-var fs = require("fs");
-var vm = require("vm");
-var express = require("express");
-var TagParser = require("./lib/TagParser.js");
-
-var tagParser = new TagParser();
-tagParser.registerTag(require("./lib/tags/print.js"));
-tagParser.registerTag(require("./lib/tags/printif.js"));
-tagParser.registerTag(require("./lib/tags/printunless.js"));
+var Jikes = require("./lib/Jikes.js");
+var jikes = new Jikes(basePath);
+jikes.listen(80);
 
 console.log("Started jikes @ "+basePath);
-
-var app = express();
-
-app.use(function(req, res, next) {
-  res.header("Content-Type", "text/html");
-  next();
-});
-
-app.get("/", function(req, res) {
-  tryRunPage("/index", req, res);
-});
-
-app.get("/*", function(req, res) {
-  tryRunPage(req.path, req, res);
-});
-
-function tryRunPage(page, req, res) {
-  var filePath = basePath+"/pages"+page;
-  tryRunJavascriptPage(filePath, req, res, function(error1) {
-    if(error1) {
-      tryRunHtmlPage(filePath, req, res, function(error2) {
-        if(error2) {
-          runErrorPage(error1, error2, req, res);
-	}
-      });
-    }
-  });
-}
-
-function tryRunJavascriptPage(page, req, res, callback) {
-  fs.readFile(page+".js", function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    var module = {};
-    var context = vm.createContext({
-      module: {}
-    });
-    try {
-      vm.runInContext(data, context);
-      context.module.exports.call({
-        query: req.query
-      }, function(viewToRender, variables) {
-        fs.readFile(page+"/../../views/"+viewToRender, function(err, data) {
-          if(err) {
-            return callback(err);
-	  }
-	  parseViewWithVariables(data.toString(), variables, function(error, result) {
-	    res.end(result);
-	    callback();
-	  });
-	});
-      });
-    }
-    catch(e) {
-      console.log("error", e);
-      return callback(e);
-    }
-  });
-}
-
-function parseViewWithVariables(raw, variables, callback) {
-  var result = tagParser.parseView(raw, variables);
-  callback(null, result); 
-}
-
-function tryRunHtmlPage(page, req, res, callback) {
- fs.readFile(page+".htm", function(err, data) {
-    if(err) {
-      return callback(err);
-    }
-    res.end(data);
-    callback();
-  });
-}
-
-function runErrorPage(error1, error2, req, res) {
-  res.end("Failed to read the file<br>"+JSON.stringify(error1)+"<br>"+JSON.stringify(error2));
-}
-
-app.listen(80);
 
